ca4dd41215fbc1d4819e9f6b583d5ef6a369cdb2
RafMiller <Rafelmillersilva@gmail.com>
2023-03-13 18:03:28 -0300

    criando a classe livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,6 +8,9 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro("300", 400);
+
+        System.out.println(livro1);
 
 
         /* int a = 5;
@@ -16,3 +19,38 @@ public class PrimeiroPrograma {
         System.out.println("Hello World de bosta!!! "(a+b));*/
     }
 }
+
+    class Livro{
+    private String nome;
+    private Integer numPag;
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPag() {
+            return numPag;
+        }
+
+        public void setNumPag(Integer numPag) {
+            this.numPag = numPag;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPag=" + numPag +
+                    '}';
+        }
+
+        public Livro(String nome, Integer numPag){
+        this.nome = nome;
+        this.numPag = numPag;
+
+     }
+    }
